f09172933f3364fd72083064694a0fad452666fa
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-02 09:54:25 +0000

    sjsj

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
 if uploaded_file is not None:
     pdata = pd.read_csv(uploaded_file).dropna()
     # pdata.columns = pdata.columns.astype(str)
-    # pdata = pdata.columns.astype(str)
+    pdata = pdata.drop(columns=['bp_label'])
     pdata['timestamp'] = pd.to_datetime(pdata['timestamp'])
     pdata['hour'] = pdata['timestamp'].dt.hour
     pdata['weekday'] = pdata['timestamp'].dt.weekday
@@ -27,11 +27,11 @@ if uploaded_file is not None:
     text_features = vectorizer.fit_transform(pdata['tweet'])
 
     pfeature_matrix = pd.concat([pd.DataFrame(text_features.toarray()), pdata[['hour', 'weekday']]], axis=1)
-    pfeature_matrix['label'] = pdata['bp_label']
+    # pfeature_matrix['label'] = pdata['bp_label']
     pft = pfeature_matrix.dropna()
     # pft =pft.columns.astype(str)
     # Use the trained model to predict the chances of a new patient having bipolar disorder
-    new_tweet_history_vec = pft.drop(columns=['label'])
+    new_tweet_history_vec = pft
     # inputvect = new_tweet_history_vec.columns.astype(float)
     with open('model.pkl', 'rb') as f:
         model = pickle.load(f)
