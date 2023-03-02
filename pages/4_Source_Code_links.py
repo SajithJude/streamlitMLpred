1f12beb9710d25fc0009fa1757d8b9dc29d33d8a
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-02 11:48:22 +0000

    sss

--- pages/4_Source_Code_links.py
+++ pages/4_Source_Code_links.py
@@ -1,2 +1,13 @@
 import Streamlit as st
 
+# lst = ['BERT Model', 'Preprocessing', 'Visualzing', 'Machine learning']
+# url = ['https://colab.research.google.com/drive/1A4tVWVlJXLqGkSXxs4UH48rLRSZRLhxn?usp=sharing','https://colab.research.google.com/drive/1A4tVWVlJXLqGkSXxs4UH48rLRSZRLhxn?usp=sharing','https://colab.research.google.com/drive/1A4tVWVlJXLqGkSXxs4UH48rLRSZRLhxn?usp=sharing','https://colab.research.google.com/drive/1A4tVWVlJXLqGkSXxs4UH48rLRSZRLhxn?usp=sharing']
+# url = st.selectbox('Select The Colab Implementation You want to see', lst)
+
+
+# st.iframe
+
+st.markdown("[BERT MODEL Deep learning  https://colab.research.google.com/drive/1bxDGTDPx0lZ3e2L3L_ZI2SEjh96KfCiH?usp=sharing")
+st.markdown("[Preprocessing Dataset https://colab.research.google.com/drive/13zJDeMBXQgp1eTOwAEBPGEpQqTEN00TB?usp=sharing")
+st.markdown("[Visualizinghttps://colab.research.google.com/drive/15j6SFRLdbFVyZRMKpas4oXZi1X9vr4sY?usp=sharing")
+st.markdown("[Machine learning https://colab.research.google.com/drive/1kI2u2PCaLam2nJaxq-VwfZTujlOALYul?usp=sharing")
